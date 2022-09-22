ebecf532e94fedf68ed19788257b9d954bd8692d
heeyoung8748 <103082973+heeyoung8748@users.noreply.github.com>
2022-09-22 14:24:37 +0900

    [Feat] BackgroundScrolling
    배경 왼쪽으로 이동하는 처리

---
+++ IcePENG/Assets/Scripts/ScrollingObjects.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObjects : MonoBehaviour
+{
+    public float MoveSpeed = 15f; // 이동 속도
+
+    private void Update()
+    {
+        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+        transform.Translate(-1 * MoveSpeed * Time.deltaTime, 0f, 0f);
+
+    }
+
+}
