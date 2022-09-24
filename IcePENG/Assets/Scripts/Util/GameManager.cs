4d3c4e18dd5fcd0b9ed85d8e931bfa53fa84fd59
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-09-25 00:02:16 +0900

    [Add] 싱글톤 추가

---
+++ IcePENG/Assets/Scripts/Util/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // life system
+    // game key
+    // score system
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
