b03540575435970280e70d9b5d5a5d52a5a0ce6a
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-09-25 00:50:03 +0900

    [Chore] 게임 매니저 수정
    - 뭘 해놨다기에도 애매하다..

--- IcePENG/Assets/Scripts/Util/GameManager.cs
+++ IcePENG/Assets/Scripts/Util/GameManager.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SingletoBehavior<GameManeger>
 {
     // life system
     // game key
     // score system
-    void Start()
-    {
-        
-    }
 
-    void Update()
+#region StartGame
+    // 플레이어가 깃발 지나갔을 때
+#endregion
+
+#region Score
+    public UnityEvent UpdateScore = new UnityEvent();
+    
+    public int CurrentScore = 0;
+    public int BestScore = -100;
+#endregion
+
+    public void GameStart()
     {
-        
+        // 게임 키 설명 UI 지우고
+        // 게임 플렛폼 움직이도록
     }
 }
