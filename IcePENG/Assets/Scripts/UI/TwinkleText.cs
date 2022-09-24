f3f28ac39e5f2d760863506cb3ac0377e1c224a1
heeyoung <heeyoung8748@gmail.com>
2022-09-25 00:06:44 +0900

    [Add] 글자 깜박임 효과 및 씬 전환 기능 추가
    - 글씨는 0.5초마다 꺼지거나 / 켜진다
    - 엔터 키를 누르면 인게임 화면으로 이동한다

--- IcePENG/Assets/Scripts/UI/TwinkleText.cs
+++ IcePENG/Assets/Scripts/UI/TwinkleText.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TwinkleText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float _elapsedTime = 0f;
+    private bool _isTextTurnoff = false;
+    private TextMeshProUGUI _ui;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        _ui = GetComponent<TextMeshProUGUI>();   
+    }
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("LevelDesign_stage1");
+        }
+        _elapsedTime += Time.deltaTime;
+        if(_elapsedTime >= 0.5f)
+        {
+            _isTextTurnoff = !_isTextTurnoff;
+            _elapsedTime = 0f;
+        }
+        if(_isTextTurnoff)
+        {
+            _ui.text = " ";
+        }
+        else
+        {
+            _ui.text = "PRESS ENTER TO START";
+        }
+
     }
 }
