94a092dcb318d5e666ca5e4ad9ddd5ff90eb8ce8
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-09-26 21:20:45 +0900

    [Feat] Life system UI 구현

--- IcePENG/Assets/Scripts/UI/Life.cs
+++ IcePENG/Assets/Scripts/UI/Life.cs
@@ -1,39 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Life : MonoBehaviour
 {
-    private int lifeCount = 0;
+    private int lifeCount = 3;
+
+    public Sprite LifeSprite;
+    public Sprite EmptyLifeSprite;
+
+    public GameObject LifeBar;
+    public GameObject[] Lifes = new GameObject[5];
+
+    private Image _image;
 
     void Start()
     {
         GameManager.Instance.LifeChange.RemoveListener(LifeOperation);
         GameManager.Instance.LifeChange.AddListener(LifeOperation);
+/*
+        for (int i = 0; i < 5; ++i)
+        {
+            Lifes[i] = LifeBar.transform.GetChild(i).Image;
+            _image = Lifes[i].GetComponent<Image>();
+
+            if (i < lifeCount)
+            {
+                Lifes[i]._image = LifeSprite;
+            }
+            else
+            {
+                Lifes[i]._image = EmptyLifeSprite;
+            }
+
+            Debug.Log($"{Lifes[i]}");
+        }
+*/
     }
 
-    public void LifeOperation(int life)
+    public void LifeOperation(int elapseLife)
     {
         // life와 lifeCount가 같을 때, 혹은 lifeCount가 이미 최대일 때 return; <- 이건 나중에
-        if (life == lifeCount)
+        if (elapseLife == lifeCount)
         {
             return;
         }
 
-        // 증가일 때
-        if (life < lifeCount)
+        _image = Lifes[elapseLife].GetComponent<Image>();
+
+        // 감소
+        if (elapseLife < lifeCount)
         {
-            // 하트 채우고
-            //
+            // 하트 지우기
+            _image.sprite = EmptyLifeSprite; 
+
             return;
         }
 
-        // 감소일 때
-        if (life > lifeCount)
+        // 증가
+        if (elapseLife > lifeCount)
         {
-            // 하트 지우고
-            
+            // 하트 채우기
+            _image.sprite = LifeSprite;
+
             return;
         }
+
+        lifeCount = elapseLife;
     }
 }
